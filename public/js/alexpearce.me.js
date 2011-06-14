a435ad9f424469ace83b82afe1f296420a7f33ce
Alex Pearce <alex@alexpearce.me>
2011-06-14 01:03:06 +0100

    Reinclude the last.fm script. Add navigation highlighting.

--- public/js/alexpearce.me.js
+++ public/js/alexpearce.me.js
@@ -80,7 +80,7 @@ $(function() {
   	fetchArtist();
   }
   function fetchArtist() {
-  	$.get('assets/php/lastfm.php', [], function(data) {
+  	$.get('/php/lastfm.php', [], function(data) {
   		var recentTracks = data.recenttracks;
   		// we check to see if track is an array
   		// as if I'm currently scrobbling, the zeroth
